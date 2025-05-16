ec51024ee569e1e2fc50071fcdbe003f242e83f7
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-16 14:01:51 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import AppPage from './components/AppPage';
+import LandingPage from './LandingPage';
+import AppPage from './AppPage';
 
 const App = () => {
   return (
