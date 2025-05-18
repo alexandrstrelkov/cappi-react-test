12976a93c3fd52e1aa56228dbb10c4f7b735978f
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-18 12:27:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./style.css";
+import "./style.css";  // Подключение style.css
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
-import AppPage from "./components/AppPage";
+import LandingPage from "./LandingPage";
+import AppPage from "./AppPage";
 
 function App() {
   return (
