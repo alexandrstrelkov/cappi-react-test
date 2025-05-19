6eb8521f1dc4d8e8d176405dc9100ee7e5f6d5bf
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-19 19:03:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./output.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";  // Подключение index.css
+//import "./index.css";  // Подключение index.css
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
