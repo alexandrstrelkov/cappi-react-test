543b13b74e2605833e44d41e9fc11e674ef70a08
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-20 22:35:07 +0300

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -6,7 +6,6 @@ import ProfitChart from "./ProfitChart";
 const Dashboard = ({ chartData }) => {
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">График доходности</h2>
       <ProfitChart data={chartData} />
     </div>
   );
